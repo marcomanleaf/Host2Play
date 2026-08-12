abb59e4b00115bdcda25b2fe3867fb4e39fe83c4
marcomanleaf <13471151256@163.com>
2026-08-12 21:14:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=65bd476a-deb6-4585-87fb-7adbe34809e8",
+    "https://host2play.gratis/server/renew?i=54fffad1-a71b-4e4d-8af3-0e4efb4480f1",
     # 添加更多链接
 ]
 
